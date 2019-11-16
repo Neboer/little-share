829fda7af03d232c2e15bfb0fde0ee1c87e766e5
neboer <rubinposter@gmail.com>
2019-11-16 18:02:12 +0800

    这次，文件大小会很准确的反映出来了。

--- front/transmit.js
+++ front/transmit.js
@@ -1,6 +1,7 @@
-function update_progressBar_and_text(index, value) {
+function update_progressBar_and_text(index, value, max_value) {
     let bar = document.getElementsByClassName("bar")[index];
     let tex = document.getElementsByClassName("lint")[index];
+    bar.max = max_value;
     bar.value = value;
     let percent = value * 100 / bar.max;
     tex.innerText = percent.toFixed(1) + "%"
@@ -15,7 +16,7 @@ function get_max_spare_space() {
 function upload_one_file_to_server(file, index) {
     return axios.post("/upload", file, {
         onUploadProgress: function (progressEvent) {
-            update_progressBar_and_text(index, progressEvent.loaded);
+            update_progressBar_and_text(index, progressEvent.loaded, progressEvent.total);
         }
     }).then(() => {
     }, (err) => {
