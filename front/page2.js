5c0ac7f9521c460607b45e2a59b1ec4642860021
neboer <rubinposter@gmail.com>
2019-12-01 17:33:02 +0800

    实现了基本所有的功能 １．需要找bug ２．需要美化

--- front/page2.js
+++ front/page2.js
@@ -1,16 +1,45 @@
 // 我决定拥抱时代，使用JQuery
-$(() => {
+function update_server_info() {
     $.get('/maxSpace', (responseTxt) => {
         $("#space").text(bytes_to_readable_string(responseTxt))
     });
 
+    $.getJSON('/files', (data) => {
+        refill_server_files_list(data, $("#files_list_table"), () => {
+            update_server_info();
+        })
+    });
+}
+
+$(() => {
+    update_server_info();
     $("#up_input").change(() => {
         $.get('/maxSpace', (responseTxt) => {
+            let server_max_size = parseInt(responseTxt);
             let files = $("#up_input").prop('files');
-            if (file_size_check_allow(files, 5e7,parseInt(responseTxt))){
+            if (file_size_check_allow(files, 5e7, server_max_size)) {
                 //　文件大小校验通过。
-
+                refresh_upload_files_table(files, server_max_size, $("#files_table"))
             }
         })
+    });
+
+    $("#sm_button").click(() => {
+        let files = $('#up_input').prop('files');
+        for (let index in files) {
+            if (files.hasOwnProperty(index)) {
+                upload_single_file_to_server(files[index], (progressEvent) => {
+                    //　箭头函数!!!!箭头函数中的this不能够提供我们想要的功能!
+                    let block_need_update = $("td:last-child").filter(function () {
+                        return $(this).siblings(":first").text() === files[index].name
+                    });
+                    let text = block_need_update.children(":last");
+                    let pbar = block_need_update.children(":first");
+                    text.text((progressEvent.loaded / progressEvent.total * 100).toFixed(1) + '%');
+                    pbar.prop("max", progressEvent.total);
+                    pbar.val(progressEvent.loaded);
+                }).then(update_server_info, () => {alert("上传异常！")})
+            }
+        }
     })
 });
--- front/tools.js
+++ front/tools.js
@@ -11,9 +11,9 @@ function bytes_to_readable_string(size) {
 
 function file_size_check_allow(files, maxSizeEach, maxSizeTotal) {
     let total_size = 0;
-    for(let i in files){
-        if (files.hasOwnProperty(i)){
-            if (files[i].size > maxSizeEach){
+    for (let i in files) {
+        if (files.hasOwnProperty(i)) {
+            if (files[i].size > maxSizeEach) {
                 return false
             }
             total_size += files[i].size
@@ -22,10 +22,84 @@ function file_size_check_allow(files, maxSizeEach, maxSizeTotal) {
     return total_size < maxSizeTotal;
 }
 
-function generate_file_form(files, serverMaxTotalSize) {
-    let current_size = 0;
-    for (let file of files){
-        let line = "<tr><td>" + file.name + "</td></tr>" + bytes_to_readable_string(file.size) +
+function calculate_last_time_seconds(size, spare_space_bytes) {
+    let store_for_days = spare_space_bytes / size;
+    if (store_for_days > 400) {
+        return null;// forever.
+    } else {
+        return Math.floor(store_for_days * 24 * 3600)
+    }
+}
 
+
+function seconds_to_readable(seconds) {
+    // TIP: to find current time in milliseconds, use:
+    // let  current_time_milliseconds = new Date().getTime();
+    if (!seconds) {
+        return "永久"
+    }
+
+    function numberEnding(number) {
+        return (number > 1) ? 's' : '';
+    }
+
+    let years = Math.floor(seconds / 31536000);
+    if (years) {
+        return years + ' year' + numberEnding(years);
+    }
+    //TODO: Months! Maybe weeks?
+    let days = Math.floor((seconds %= 31536000) / 86400);
+    if (days) {
+        return days + ' day' + numberEnding(days);
+    }
+    let hours = Math.floor((seconds %= 86400) / 3600);
+    if (hours) {
+        return hours + ' hour' + numberEnding(hours);
+    }
+    let minutes = Math.floor((seconds %= 3600) / 60);
+    if (minutes) {
+        return minutes + ' minute' + numberEnding(minutes);
+    }
+    return 'less than a minute'; //'just now' //or other string you like;
+}
+
+function append_files_to_upload_form(files, serverMaxTotalSize, form) {
+    let current_size = 0;
+    for (let file of files) {
+        let line = "<tr><td>" + file.name + "</td>" + "<td>" + bytes_to_readable_string(file.size)
+            + "</td>" + "<td>" + seconds_to_readable(calculate_last_time_seconds(file.size, serverMaxTotalSize - current_size)) + "</td>"
+            + "<td><progress class='bar'></progress><small class='percent'>waiting...</small></td></tr>";
+        form.append(line);
+        current_size += file.size;
+    }
+}
+
+function upload_single_file_to_server(file, onUploadProgressCallBack) {
+    let formData = new FormData();
+    formData.append('file', file);
+    return axios.post('/upload', formData, {onUploadProgress: onUploadProgressCallBack})
+}
+
+function refresh_upload_files_table(files, server_max_size, files_table) {
+    files_table.find("tr:gt(0)").remove();
+    append_files_to_upload_form(files, server_max_size, files_table)
+}
+
+function refill_server_files_list(server_files_json, server_files_table, deleteCallBack) {
+    server_files_table.find("tr:gt(0)").remove();
+    for (let single_file_info of server_files_json) {
+        let line = "<tr><td>" + single_file_info["FileName"] + "</td><td>" + bytes_to_readable_string(single_file_info["FileSizeBytes"]) + "</td><td>"
+            + seconds_to_readable(single_file_info["FileSurplusKeepSeconds"]) + "</td></tr>";
+        let download_label = $("<a>下载</a>").prop('href', '/files/' + single_file_info["FileName"]);
+        let delete_label = $("<a>删除</a>").prop('href', 'javascript:void(0)');
+        delete_label.click(() => {
+            axios.delete('/files/' + single_file_info["FileName"]).then(() => {
+                alert("删除成功");
+            }, () => {
+                alert("删除失败！");
+            }).then(deleteCallBack)
+        });
+        let prepared_line = $(line).append(download_label, delete_label);
+        server_files_table.append(prepared_line)
     }
 }
