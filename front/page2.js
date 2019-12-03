49b47a0b632e5a495696b509f0e67ff095ef50fc
neboer <rubinposter@gmail.com>
2019-12-03 21:37:29 +0800

    Revert "因为要部署到服务器，所以前端改了一个URLPrefix"
    This reverts commit 2f31ae66

--- front/page2.js
+++ front/page2.js
@@ -1,12 +1,10 @@
 // 我决定拥抱时代，使用JQuery
-const URLPrefix = "";
-
 function update_server_info() {
-    $.get(URLPrefix + '/maxSpace', (responseTxt) => {
+    $.get('/maxSpace', (responseTxt) => {
         $("#space").text(bytes_to_readable_string(responseTxt))
     });
 
-    $.getJSON(URLPrefix + '/files', (data) => {
+    $.getJSON('/files', (data) => {
         refill_server_files_list(data, $("#files_list_table"), () => {
             update_server_info();
         })
